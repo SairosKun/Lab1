2be58fb89bbfea8c79dbff1075b5304346f2f462
Níkolaz Dupont <nikolaz.dupont@gmail.com>
2023-08-20 23:45:51 -0300

    Realocação de exercicios e novos exericios avançados
    Realocação dos exercícios para uma nova pasta específica e novos exercicios um pouco mais avançados de cálculo básico, porcentagem e bhaskara

---
+++ Exerc_Avanc_Sem1/Exercicio_Avanc_Lab1.py
@@ -0,0 +1,8 @@
+A = float(input("Informe o valor de A: "))
+B = int(input("Informe o valor de B: "))
+print("O valor de A multiplicado por B é: ", A*B)
+print("O valor de A dividido por B é: " ,A/B, "Mas em decimais fica: %.2f" % (A/B), "!")
+print("O valor de A mais B é: ", A+B, "E o valor de A menos B é: ", A-B)
+print("O valor de A elevado à B é: " , A**B)
+
+#Potencia: Num1**Num2; Radiaciação: Num1**(1/Num2) -> parenteses dão precedencia. 25**(1/2)->raiz quadrada de 25 | 27**(1/3)->rais cubica de 27
